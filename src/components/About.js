4accdc8ee8644eb9a3428019b47f96bdf39ad40f
Amanda Mokhachane <AMokhachane@SINGULAR.CO.ZA>
2024-12-17 13:50:39 +0200

    last save

--- src/components/About.js
+++ src/components/About.js
@@ -1,11 +1,28 @@
 import React, { useState, useEffect } from "react";
+import styles from './About.module.css';
 
 export const About = () => {
 
 	return (
-		<div>
-			this is a About page
-		</div>
+		<div className={styles.loginContainer}>
+      <div className={styles.left}>
+        <div className={styles.logo}>
+          <div className={styles.logoSquare}></div>
+          <span className={styles.boldText}>Amanda</span>
+        </div>
+		
+		<div className={styles.logo}>
+			<span className={styles.boldText}>Mokhachane</span>
+        </div>
+        
+      </div>
+
+      <div className={styles.rightSide}>
+        <div className={styles.iconGrid}>
+        <img src="https://res.cloudinary.com/drgxphf5l/image/upload/v1734435509/160619183_gnnic9.jpg" className='image' />
+        </div>
+      </div>
+    </div>
 	);
 };
 
